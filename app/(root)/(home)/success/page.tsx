2c600a317cab567c1d8e19d9df6e3f090105ddcc
Aseem5047 <aseemgupta2002@gmail.com>
2024-06-22 17:32:29 +0530

    updated success component

--- app/(root)/(home)/success/page.tsx
+++ app/(root)/(home)/success/page.tsx
@@ -1,35 +1,8 @@
-"use client";
+import Success from "@/components/shared/Success";
+import React from "react";
 
-import { success } from "@/constants/icons";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
-
-// Update the Success component to use the defined props type
-const Success = ({
-	redirect,
-	event,
-}: {
-	redirect?: string;
-	event?: string;
-}) => {
-	const router = useRouter();
-	useEffect(() => {
-		setTimeout(() => {
-			router.push(`/${redirect ? redirect : "payment"}`);
-		}, 4000);
-	}, [router]);
-
-	return (
-		<div className="flex flex-col items-center justify-center min-w-full h-full gap-7">
-			{success}
-			<div className="flex flex-col items-center justify-center gap-2 tracking-wider">
-				<span className="font-semibold text-xl">
-					{event ? event : "Amount Added"}
-				</span>
-				<span className="font-semibold text-lg text-green-1">Successfully</span>
-			</div>
-		</div>
-	);
+const SuccessPage = () => {
+	return <Success />;
 };
 
-export default Success;
+export default SuccessPage;
