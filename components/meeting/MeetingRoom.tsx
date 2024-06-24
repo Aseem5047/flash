36f98e2e4eb6a88c5b3e30f613c46f7011edc4f1
Aseem5047 <aseemgupta2002@gmail.com>
2024-06-24 16:46:45 +0530

    updated call controls

--- components/meeting/MeetingRoom.tsx
+++ components/meeting/MeetingRoom.tsx
@@ -16,7 +16,6 @@ import {
 
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
-import { useSearchParams } from "next/navigation";
 import { SwitchCamera, Users } from "lucide-react";
 import EndCallButton from "../calls/EndCallButton";
 import { useUser } from "@clerk/nextjs";
@@ -27,6 +26,8 @@ import useWarnOnUnload from "@/hooks/useWarnOnUnload";
 import { VideoToggleButton } from "../calls/VideoToggleButton";
 import { AudioToggleButton } from "../calls/AudioToggleButton";
 import SinglePostLoader from "../shared/SinglePostLoader";
+import SwitchCameraType from "../calls/SwitchCameraType";
+import AudioDeviceList from "../calls/AudioDeviceList";
 
 type CallLayoutType = "grid" | "speaker-bottom";
 
@@ -195,50 +196,16 @@ const MeetingRoom = () => {
 				</SpeakingWhileMutedNotification>
 
 				{isMobile && (
-					<>
-						<button
-							className="p-3 bg-[#ffffff14] rounded-full hover:bg-[#4c535b] transition-all duration-300 hover:scale-110"
-							onClick={() => setShowAudioDeviceList((prev) => !prev)}
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								strokeWidth={1.5}
-								stroke="currentColor"
-								className="size-6"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"
-								/>
-							</svg>
-						</button>
-
-						{showAudioDeviceList && (
-							<div
-								className="absolute bottom-16 left-0 bg-dark-1 rounded-t-xl w-full z-40"
-								onChange={() => setShowAudioDeviceList(false)}
-							>
-								<DeviceSelectorAudioOutput title="Audio Out" />
-								<DeviceSelectorAudioInput title="Audio In" />
-							</div>
-						)}
-					</>
+					<AudioDeviceList
+						showAudioDeviceList={showAudioDeviceList}
+						setShowAudioDeviceList={setShowAudioDeviceList}
+					/>
 				)}
 
 				{isVideoCall &&
 					(isMobile ? <VideoToggleButton /> : <ToggleVideoPublishingButton />)}
 
-				{isMobile && (
-					<button
-						onClick={toggleCamera}
-						className="p-3 bg-[#ffffff14] rounded-full hover:bg-[#4c535b] transition-all duration-300 hover:scale-110"
-					>
-						<SwitchCamera size={20} className="text-white" />
-					</button>
-				)}
+				{isMobile && <SwitchCameraType toggleCamera={toggleCamera} />}
 				<Tooltip>
 					<TooltipTrigger className="hidden md:block">
 						<button onClick={() => setShowParticipants((prev) => !prev)}>
