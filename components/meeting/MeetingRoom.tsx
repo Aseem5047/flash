7ddfaa26e3165b7e4394f698137ded3abe380d1c
Aseem5047 <aseemgupta2002@gmail.com>
2024-06-24 15:46:58 +0530

    updated meeting room tooltips

--- components/meeting/MeetingRoom.tsx
+++ components/meeting/MeetingRoom.tsx
@@ -187,16 +187,7 @@ const MeetingRoom = () => {
 				<SpeakingWhileMutedNotification>
 					{isVideoCall &&
 						(isMobile ? (
-							<>
-								<Tooltip>
-									<TooltipTrigger>
-										<AudioToggleButton />
-									</TooltipTrigger>
-									<TooltipContent className="mb-2 bg-green-1  border-none">
-										<p className="!text-white">Audio</p>
-									</TooltipContent>
-								</Tooltip>
-							</>
+							<AudioToggleButton />
 						) : (
 							<ToggleAudioPublishingButton />
 						))}
@@ -204,32 +195,25 @@ const MeetingRoom = () => {
 
 				{isMobile && (
 					<>
-						<Tooltip>
-							<TooltipTrigger>
-								<button
-									className="p-3 bg-[#ffffff14] rounded-full hover:bg-[#4c535b]"
-									onClick={() => setShowAudioDeviceList((prev) => !prev)}
-								>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										strokeWidth={1.5}
-										stroke="currentColor"
-										className="size-6"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"
-										/>
-									</svg>
-								</button>
-							</TooltipTrigger>
-							<TooltipContent className="mb-2 bg-green-1  border-none">
-								<p className="!text-white">Audio Out Source</p>
-							</TooltipContent>
-						</Tooltip>
+						<button
+							className="p-3 bg-[#ffffff14] rounded-full hover:bg-[#4c535b]"
+							onClick={() => setShowAudioDeviceList((prev) => !prev)}
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={1.5}
+								stroke="currentColor"
+								className="size-6"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"
+								/>
+							</svg>
+						</button>
 
 						{showAudioDeviceList && (
 							<div
@@ -244,37 +228,15 @@ const MeetingRoom = () => {
 				)}
 
 				{isVideoCall &&
-					(isMobile ? (
-						<>
-							<Tooltip>
-								<TooltipTrigger>
-									<VideoToggleButton />
-								</TooltipTrigger>
-								<TooltipContent className="mb-2 bg-green-1  border-none">
-									<p className="!text-white">Video</p>
-								</TooltipContent>
-							</Tooltip>
-						</>
-					) : (
-						<ToggleVideoPublishingButton />
-					))}
+					(isMobile ? <VideoToggleButton /> : <ToggleVideoPublishingButton />)}
 
 				{isMobile && (
-					<>
-						<Tooltip>
-							<TooltipTrigger>
-								<button
-									onClick={toggleCamera}
-									className="p-3 bg-[#ffffff14] rounded-full hover:bg-[#4c535b]"
-								>
-									<SwitchCamera size={20} className="text-white" />
-								</button>
-							</TooltipTrigger>
-							<TooltipContent className="mb-2 bg-green-1  border-none">
-								<p className="!text-white">Switch Video</p>
-							</TooltipContent>
-						</Tooltip>
-					</>
+					<button
+						onClick={toggleCamera}
+						className="p-3 bg-[#ffffff14] rounded-full hover:bg-[#4c535b]"
+					>
+						<SwitchCamera size={20} className="text-white" />
+					</button>
 				)}
 				<Tooltip>
 					<TooltipTrigger className="hidden md:block">
