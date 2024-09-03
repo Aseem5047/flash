15a963543e42297400fe9b92afcdfe726e7cebc4
Aseem5047 <aseemgupta2002@gmail.com>
2024-09-03 13:57:57 +0530

    added healthcheck api

---
+++ app/api/v1/healthCheck/route.ts
@@ -0,0 +1,40 @@
+// /app/api/healthCheck/route.ts
+import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+import { connectToDatabase } from "@/lib/database";
+import * as Sentry from "@sentry/nextjs";
+
+export async function GET() {
+	try {
+		// Check if mongoose connection is ready
+		const isDbConnected = mongoose.connection.readyState === 1;
+
+		if (isDbConnected) {
+			return NextResponse.json({ status: "ok", db: "connected" });
+		} else {
+			// Attempt to reconnect to the database
+			try {
+				await connectToDatabase(); // Attempt to reconnect using your dbConnect function
+				return NextResponse.json({ status: "ok", db: "reconnected" });
+			} catch (reconnectError: any) {
+				Sentry.captureException(reconnectError);
+
+				return NextResponse.json(
+					{
+						status: "error",
+						db: "reconnect_failed",
+						error: reconnectError.message,
+					},
+					{ status: 500 }
+				);
+			}
+		}
+	} catch (error: any) {
+		Sentry.captureException(error);
+
+		return NextResponse.json(
+			{ status: "error", error: error.message },
+			{ status: 500 }
+		);
+	}
+}
