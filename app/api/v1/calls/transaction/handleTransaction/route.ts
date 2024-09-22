746bd29553b3ee90ee963cd4c64950b3f80df683
Aseem5047 <aseemgupta2002@gmail.com>
2024-09-22 16:13:22 +0530

    updated logic

--- app/api/v1/calls/transaction/handleTransaction/route.ts
+++ app/api/v1/calls/transaction/handleTransaction/route.ts
@@ -2,7 +2,7 @@
 import { transactionHandler } from "@/utils/TransactionHandler";
 import { NextResponse } from "next/server";
 
-export default async function POST(request: Request) {
+export async function POST(request: Request) {
 	const { callId, expertId, clientId, duration, isVideoCall } =
 		await request.json();
 
