d3fd3577535f009b5399bab5f246618f8c0bd57c
Aseem5047 <aseemgupta2002@gmail.com>
2024-11-06 15:22:38 +0530

    updated changes

--- components/calls/CallListMobileCreator.tsx
+++ components/calls/CallListMobileCreator.tsx
@@ -82,11 +82,11 @@ const CallListMobileCreator = ({
 								const fullName = getDisplayName(client);
 
 								return (
-									<div
+									<section
 										key={userCall.callId}
 										className={`flex h-full w-full items-start justify-between pt-2 pb-4 xl:max-w-[568px] border-b xl:border xl:rounded-xl xl:p-4 xl:shadow-md border-gray-300 `}
 									>
-										<div className="flex  flex-col items-start justify-start w-full gap-2">
+										<section className="flex  flex-col items-start justify-start w-full gap-2">
 											{/* Expert's Details */}
 											<section className="w-1/2 flex items-center justify-start gap-4">
 												{/* client image */}
@@ -108,27 +108,82 @@ const CallListMobileCreator = ({
 													<p className="text-base tracking-wide whitespace-nowrap capitalize">
 														{fullName || "client"}
 													</p>
-													<section className="flex items-center justify-start gap-2 h-fit text-[12.5px]">
+													<section className="flex flex-col xm:flex-row items-start xm:items-center justify-start gap-1 xm:gap-2 h-fit text-[12.5px]">
 														<span className="whitespace-nowrap">Client</span>
 														<span className="text-gray-400 text-xs">|</span>
-														<span className="capitalize">{userCall.type}</span>
+
+														<span
+															className={`whitespace-nowrap ${
+																userCall.status === "Ended"
+																	? "text-green-1"
+																	: "text-red-500"
+															}`}
+														>
+															{userCall.status === "Ended"
+																? "Completed"
+																: userCall.status}
+														</span>
 													</section>
 												</div>
 											</section>
 											{/* call details */}
-											<div className="flex flex-wrap items-center justify-start gap-2 pl-16 text-[12.5px]">
+											<section className="flex flex-wrap items-center justify-start gap-2 pl-16 text-[12.5px]">
 												<span
-													className={`text-sm ${
-														userCall.status === "Ended"
-															? "text-green-1"
-															: "text-red-500"
+													className={`${
+														userCall.status === "Ended" ? "block" : "hidden"
 													}`}
 												>
-													{userCall.status === "Ended"
-														? "Completed"
-														: userCall.status}
+													{userCall.type === "audio" ? (
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															fill="none"
+															viewBox="0 0 24 24"
+															strokeWidth={1.5}
+															stroke="currentColor"
+															className="size-6"
+														>
+															<path
+																strokeLinecap="round"
+																strokeLinejoin="round"
+																d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"
+															/>
+														</svg>
+													) : userCall.type === "video" ? (
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															fill="none"
+															viewBox="0 0 24 24"
+															strokeWidth={1.5}
+															stroke="currentColor"
+															className="size-4"
+														>
+															<path
+																strokeLinecap="round"
+																strokeLinejoin="round"
+																d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z"
+															/>
+														</svg>
+													) : (
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															fill="none"
+															viewBox="0 0 24 24"
+															strokeWidth={1.5}
+															stroke="currentColor"
+															className="size-4"
+														>
+															<path
+																strokeLinecap="round"
+																strokeLinejoin="round"
+																d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+															/>
+														</svg>
+													)}
 												</span>
 												<section className="flex items-center justify-start gap-2">
+													{userCall.duration && (
+														<span className="text-gray-400">•</span>
+													)}
 													<span>
 														{userCall.duration &&
 															(() => {
@@ -160,24 +215,23 @@ const CallListMobileCreator = ({
 														</>
 													)}
 												</section>
-											</div>
-										</div>
+											</section>
+										</section>
 										{/* StartedAt & Feedbacks */}
 										<section className="relative w-fit flex flex-col items-end justify-between h-full gap-2">
 											<span className="text-sm text-[#A7A8A1] pr-2 pt-1 whitespace-nowrap">
 												{formattedDate.dateTime}
 											</span>
 
-											<section>
-												<OptionsList
-													callId={userCall.callId}
-													currentCreator={currentUser}
-													creatorId={currentUser?._id as string}
-													clientId={client?._id as string}
-												/>
-											</section>
+											<OptionsList
+												callId={userCall.callId}
+												currentCreator={currentUser}
+												creatorId={currentUser?._id as string}
+												clientId={client?._id as string}
+												userCall={userCall}
+											/>
 										</section>
-									</div>
+									</section>
 								);
 							})
 						)}
